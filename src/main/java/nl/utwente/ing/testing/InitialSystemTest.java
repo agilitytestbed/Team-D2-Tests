a26a752a38e6e72a44b74f0f774bead5022bab95
Daan Kooij <d.kooij-1@student.utwente.nl>
2018-02-26 10:57:36 +0100

    Add JUnit and create a sample test

--- src/main/java/nl/utwente/ing/testing/InitialSystemTest.java
+++ src/main/java/nl/utwente/ing/testing/InitialSystemTest.java
@@ -1,10 +1,18 @@
 package nl.utwente.ing.testing;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class InitialSystemTest {
+
+    @Test
+    public void testSessionIDRetrieval() {
+        get("/api/v1/sessions").then().statusCode(200).body("isEmpty()", Matchers.is(false));
+    }
 
 
 
