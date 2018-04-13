b8d5a942bb43ee03f83adfef1422de35dc46b511
Daan Kooij <d.kooij-1@student.utwente.nl>
2018-04-13 09:51:28 +0200

    Add test setup and sessionID retrieval test

--- src/main/java/nl/utwente/ing/testing/InitialSystemTest.java
+++ src/main/java/nl/utwente/ing/testing/InitialSystemTest.java
@@ -1,17 +1,39 @@
 package nl.utwente.ing.testing;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static io.restassured.path.json.JsonPath.from;
 import static org.hamcrest.Matchers.*;
 
 public class InitialSystemTest {
 
+    private static String sessionID;
+
+    @BeforeAll
+    public static void setup() {
+        // Get sessionID
+        String responseString = when().post("/api/v1/sessions").
+                then().contentType(ContentType.JSON).
+                extract().response().asString();
+        Map<String, ?> responseMap = from(responseString).get("");
+        sessionID = (String) responseMap.get("id");
+    }
+
     @Test
     public void testSessionIDRetrieval() {
-        get("/api/v1/sessions").then().statusCode(200).body("isEmpty()", Matchers.is(false));
+        when().post("/api/v1/sessions").
+                then().statusCode(201).
+                body("$", hasKey("id"));
     }
 
 
