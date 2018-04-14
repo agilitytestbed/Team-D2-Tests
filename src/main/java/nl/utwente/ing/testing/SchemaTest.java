2e845e59ad07d711c8fe8af9d2c715dda9db1184
Sammz <sam.witt25@gmail.com>
2018-04-14 09:58:16 +0200

    Add getsessionID to schematest

--- src/main/java/nl/utwente/ing/testing/SchemaTest.java
+++ src/main/java/nl/utwente/ing/testing/SchemaTest.java
@@ -1,19 +1,30 @@
 package nl.utwente.ing.testing;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 
 public class SchemaTest {
+
+    public static String sessionID;
+
+
+    @BeforeClass
+    public static void getSessionID() {
+        sessionID = get("/api/v1/sessions").asString();
+        System.out.println(sessionID);
+    }
 
     @Test
     public void test_GET_transactions_JSON_schema() {
 
         when().
-                get("/api/v1/transactions").
+                get("/api/v1/transactions?session_id=" + sessionID).
                 then().
-                assertThat().body(matchesJsonSchemaInClasspath("TransactionList.json"));
+                assertThat().body(matchesJsonSchemaInClasspath("Transaction.json"));
 
     }
 
@@ -21,7 +32,7 @@ public class SchemaTest {
     public void test_POST_transactions_JSON_schema() {
 
         when().
-                post("/api/v1/transactions").
+                post("/api/v1/transactions?session_id=" + sessionID).
                 then().
                 assertThat().body(matchesJsonSchemaInClasspath("Transaction.json"));
 
